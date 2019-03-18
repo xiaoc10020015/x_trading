050e78c998879a38406933880f70029098b7a67b
xiaoc10020015 <260500094@qq.com>
2019-03-18 22:13:23 +0800

    量化结构搭建

---
+++ src/xqt/base/main.py
@@ -0,0 +1,15 @@
+class Main:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def add_event(self):
+        pass
+
+    def add_module(self):
+        pass
+
+    def close(self):
+        pass
---
+++ src/xqt/tests/sample.py
@@ -0,0 +1,5 @@
+from src.xqt.base.main import Main
+
+print('启动xqt')
+xqt = Main()
+xqt.start()
