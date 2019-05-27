a5604964662c522f76a1ee0af6e426b832e8721b
徐斌 <xubin@dputech.com>
2019-05-27 16:34:21 +0800

    完成初版机器学习笔记

---
+++ src/xqt/com/manager/evt_manager.py
@@ -0,0 +1,4 @@
+from src.xqt.com.module.module import BaseModule
+class EvtManagerModule(BaseModule):
+    def __init__(self):
+        pass
---
+++ src/xqt/com/module/base.py
@@ -0,0 +1,3 @@
+class BaseModule:
+    def __init__(self, name: str):
+        self.name = name
